6175ae2c736ddb5d05fc0aa54407349432b9ed2b
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-09-10 22:43:29 +0530

    added the data stored on firebase to the Home screen with realtime update feature using onSnapshot() method of firebase

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from '../firebase/config'
+
+
+export const useCollection = (collection, query) => {
+
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        let ref = projectFirestore.collection(collection)
+
+        if (query) {
+            ref = ref.where(...query)
+        }
+
+        const unsubscribe = ref.onSnapshot((snapshot) => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+                results.push({ ...doc.data(), id: doc.id })
+            })
+
+            //update state
+            setDocuments(results)
+            setError(null)
+        }, (error) => {
+            console.log(error)
+            setError('could not fetch data')
+        })
+
+        //unsubscribe on unmount
+        return () => unsubscribe()
+
+    }, [collection])
+
+    return { documents, error }
+}
