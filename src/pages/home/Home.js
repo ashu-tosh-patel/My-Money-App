0bf5c8dbff0d3830e63081de2c9b6f743c8695cd
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-09-10 16:54:04 +0530

    Started creation of custom hook to interact with firestore database to add or delete a new document with name useFirestore

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,17 @@
-// eslint-disable-next-line
 import styles from './Home.module.css'
+
+//components
+import TransactionForm from './TransactionForm'
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className={styles.container}>
+            <div className={styles.content}>
+                transaction list
+            </div>
+            <div className={styles.sidebar}>
+                <TransactionForm />
+            </div>
         </div>
     )
 }
